dd69ca9d2a17e6ff999884305048531d3d57c604
Mary Towle <mary.towle@thrivent.com>
2021-04-07 18:04:47 -0500

    commit

--- Pybank/main.py
+++ Pybank/main.py
@@ -6,6 +6,17 @@ import csv
 # Path to collect data from the Resources folder
 csv_path = os.path.join('Resources','budget_data.csv')
 
+# Create initial variables
+total_number_months = 0
+net_total_profit_loss = 0
+#budget_profit_loss = 0
+#previous_budget_profit_loss = 0
+#greatest_profit_increase = 0
+#greatest_profit_decrease = 0
+#total_change_profit_loss = 0
+#change_profit_loss = 0
+
+
 #Read in the csv file
 with open(csv_path,'r') as csvfile:
         #split the data on commas
@@ -13,37 +24,27 @@ with open(csv_path,'r') as csvfile:
         header = next(csvreader)
 
         for row in csvreader:
-                print(row)
+                #add to the number of months as 1 for each month
+                total_number_months = total_number_months + 1
+                #print(total_number_months)
+                #read the profit or loss from this month and add to the total of profit or loss
+                net_total_profit_loss += row[1]
+                print(net_total_profit_loss)
+                #read in the change in profit loss from previous month to this month
+                #change_profit_loss = row[1] - row-1[1]
+                #add this change to the total change
+                #total_change_profit_loss = total_change_profit_loss + change_profit_loss
+                #Greatest increase in profits (date and amount) over entire period
+                #if change_profit_loss > greatest_profit_increase:
+                #        greatest_profict_increase = change_profit_loss
+                #Greatest decrease in losses (date and amount) over entire period
+                #if change_profit_loss < greatest_profit_decrease:
+                #        greatest_profit_decrease = change_profit_loss
+                
 
 #Define function and have it accept the 'profit_loss_data' as its sole parameter
 #def print_profit_loss_totals(profit_loss_data):
-        #Assign values to variables
-        budget_date = csv_path[0]
-        budget_profit_loss = csv_path[1]
-        greatest_profit_increase = 0
-        greatest_profit_decrease = 0
-        net_total_profit_loss = 0
-        change_profit_loss = 0
-
-        #Total number of months included in the dataset
-        num_rows = 0
-
-        for row in open(csv_path):
-                num_rows += 1
-                 #Total number of months included in the dataset
-                total_number_months = num_rows
-                 #Net total amount of profit/loss over the entire period
-                net_total_profit_loss = net_total_profit_loss + net_total_profit_loss
-                #Average of the changes in profit/loss over the enitre period
-                change_profit_loss = budget_profit_loss #need to subtract previous budget_profit_loss from previous record
-                #Greatest increase in profits (date and amount) over entire period
-                if change_profit_loss > greatest_profit_increase:
-                        greatest_profict_increase = change_profit_loss
-                #Greatest decrease in losses (date and amount) over entire period
-                if change_profit_loss < greatest_profit_decrease:
-                        greatest_profit_decrease = change_profit_loss
-                total_change_profit_loss = total_change_profit_loss + change_profit_loss
-
-        average_change_profit_loss = total_change_profit_loss / total_number_months
+#average_change_profit_loss = total_change_profit_loss / total_number_months
+#print(average_change_profit_loss)
 
 #%%
